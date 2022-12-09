5978077d6b1f1bb06bea19e0c2a4de53bc06fddc
shiva <116255391+shivagoud76@users.noreply.github.com>
2022-12-09 17:50:00 +0530

    Add files via upload

---
+++ 19_break_loop.py
@@ -0,0 +1,21 @@
+n=-1
+while(n<20):
+     n+=2
+     if n>8:
+         break
+print(n)
+
+n=0
+while(n<10):
+    n+=1
+    if n==4:
+        break
+print(n)
+
+n=0
+while(n<20):
+    n+=2
+if(n==10):
+
+#  break
+  print(n)
